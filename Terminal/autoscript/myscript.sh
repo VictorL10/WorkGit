ad109bac33b781e7b856052d1e44563c9e929a84
Victor <MyTest129867@gmail.com>
2023-08-04 12:22:53 +0300

    Terminal

---
+++ Terminal/autoscript/myscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ./autoscript #Зайти в папку 
+mkdir folder1 folder2 folder3 #Создать 3 папки
+cd folder1 #Зайти в любоую папку
+touch file1.txt file2.txt file3.txt proba1.json proba2.json #Создать 5 файлов
+mkdir sw1 sw2 sw3 # Создать 3 папки
+ls -la #Вывести список содержимого папки
+mv file1.txt file2.txt ../folder2 #переместить любые 2 файла, которые вы создали, в любую другую папку
+
+
+
